4d742be46858cb44dccf58332115fa656e878883
aznoqmous <paul.landreau931v@gmail.com>
2020-03-06 12:44:05 +0100

    added onfileselect return false prevent readFile

--- src/input-file-reader.js
+++ src/input-file-reader.js
@@ -1,63 +1,66 @@
 export default class InputFileReader{
-  constructor(config){
-    this.config = Object.assign({
-      input: null, // must be declared on class instantiation
-      decimals: 0,
+    constructor(config){
+        this.config = Object.assign({
+            input: null, // must be declared on class instantiation
+            decimals: 0,
 
-      // callbacks
-      utf8: false,
-      onFileSelect: null,
-      onProgress: null,
-      onLoaded: null
-    }, config)
+            // callbacks
+            utf8: false,
+            onFileSelect: null,
+            onProgress: null,
+            onLoaded: null
+        }, config)
 
-    this.bind()
-  }
-
-  bind(){
-    if(!this.config.input) console.warn('no input !')
-    this.config.input.addEventListener('change', (e)=>{
-      if(e.target.files.length) {
-        this.handleFileSelect(e.target.files[e.target.files.length-1])
-      }
-    })
-  }
-
-  handleFileSelect(f){
-    if(this.config.onFileSelect) this.config.onFileSelect(f)
-    this.readFile(f)
-  }
-
-  readFile(file){
-    let reader = new FileReader()
-    reader.onprogress = (e)=>{
-      if(this.config.onProgress) this.config.onProgress({
-        loaded: e.loaded,
-        total: e.total,
-        percent: Math.floor(e.loaded / e.total * Math.pow(10, this.config.decimals + 2) ) * 100 / Math.pow(10, this.config.decimals + 2) + '%'
-      })
+        this.bind()
     }
-    reader.onloadend = (e)=>{
-      if (e.target.readyState == FileReader.DONE) {
-        if(this.config.onLoaded) this.config.onLoaded(e.srcElement.result)
-      }
+
+    bind(){
+        if(!this.config.input) console.warn('no input !')
+        this.config.input.addEventListener('change', (e)=>{
+            if(e.target.files.length) {
+                this.handleFileSelect(e.target.files[e.target.files.length-1])
+            }
+        })
     }
-    if(this.config.utf8) reader.readAsText(file)
-    else reader.readAsBinaryString(file)
-  }
+
+    handleFileSelect(f){
+        let proceed = true
+        if(this.config.onFileSelect) proceed = this.config.onFileSelect(f)
+        if(proceed === false) return false
+
+        this.readFile(f)
+    }
+    
+    readFile(file){
+        let reader = new FileReader()
+        reader.onprogress = (e)=>{
+            if(this.config.onProgress) this.config.onProgress({
+                loaded: e.loaded,
+                total: e.total,
+                percent: Math.floor(e.loaded / e.total * Math.pow(10, this.config.decimals + 2) ) * 100 / Math.pow(10, this.config.decimals + 2) + '%'
+            })
+        }
+        reader.onloadend = (e)=>{
+            if (e.target.readyState == FileReader.DONE) {
+                if(this.config.onLoaded) this.config.onLoaded(e.srcElement.result)
+            }
+        }
+        if(this.config.utf8) reader.readAsText(file)
+        else reader.readAsBinaryString(file)
+    }
 
 
-  onFileSelect(callback){
-    this.config.onFileSelect = callback
-    return this
-  }
-  onProgress(callback){
-    this.config.onProgress = callback
-    return this
-  }
-  onLoaded(callback){
-    this.config.onLoaded = callback
-    return this
-  }
+    onFileSelect(callback){
+        this.config.onFileSelect = callback
+        return this
+    }
+    onProgress(callback){
+        this.config.onProgress = callback
+        return this
+    }
+    onLoaded(callback){
+        this.config.onLoaded = callback
+        return this
+    }
 
 }
